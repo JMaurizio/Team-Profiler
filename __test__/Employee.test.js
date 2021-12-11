72248404c3b6af116821c5cb10450bcabb97fada
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2021-12-10 22:09:53 -0600

    Employee passing tests

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,6 +1,3 @@
-const { expect } = require("@jest/globals");
-const { test } = require("jest-circus");
-const { exp } = require("prelude-ls");
 const Employee = require("../lib/Employee");
 
 test("Can create new employee object", () => {
@@ -9,19 +6,19 @@ test("Can create new employee object", () => {
 });
 
 test("Set employee name", () => {
-    const name = "Name";
+    const name = "name"
     const employee = new Employee(name);
     expect(employee.getName()).toBe(name);
 });
 
 test("Set employee id", () => {
-    const employeeId = 0;
-    const employee = new Employee(employeeId);
-    expect(employee.setId()).toBe(employeeId);
+    const id = 0
+    const employee = new Employee("name",id);
+    expect(employee.setId()).toBe(id);
 });
 
 test("Set employee email", () => {
-    const email = "test@test.com";
-    const employee = new Employee(email);
+    const email = "test@test.com"
+    const employee = new Employee("name", 0, email);
     expect(employee.getEmail()).toBe(email);
 })
